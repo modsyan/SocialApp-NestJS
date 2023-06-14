54dde36f1ba23a2685e0874a2de69ff6be166b70
Muhammed Hamdy <modclover7@gmail.com>
2023-06-14 20:34:22 +0300

    change salt to rounds temporarily

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
-import { UserModule } from "./user/user.module";
-import { BookmarkModule } from "./bookmark/bookmark.module";
 import { DatabaseModule} from "./database/database.module";
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, DatabaseModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [],
   providers: []
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,66 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException
+} from "@nestjs/common";
 import { DatabaseService } from "src/database/database.service";
 import { UserLoginDto, UserRegisterDto } from "./dto";
+import * as bcrypt from "bcrypt";
+import { isObjectLiteralElementLike } from "typescript";
+import { ok } from "assert";
 import { User } from "@prisma/client";
-import { last } from "rxjs";
 
 @Injectable()
 export class AuthService {
   constructor(private _dbContext: DatabaseService) {}
 
-  login(dto: UserLoginDto) {
+  async login(dto: UserLoginDto) {
     const { email, password } = dto;
-    // this._dbContext.user.create();
-    return { data: dto };
+
+    const user: User | null = await this._dbContext.user.findFirst({
+      where: { email: email }
+    });
+
+    if (!user) {
+      throw new NotFoundException(
+        user,
+        `user with email ${email} not found, please try again.`
+      );
+    }
+    const isMatch = bcrypt.compareSync(password, user.password);
+
+    if (!isMatch)
+      throw new UnauthorizedException();
+
+    return {
+      success: true,
+      message: "Login Successfully",
+      data: {user}
+    };
   }
 
-  register(dto: UserRegisterDto) {
+  async register(dto: UserRegisterDto) {
     const { email, username, firstName, lastName, password } = dto;
-   
-    return dto;
+    //TODO: helper to validate environment variables
+    // const salt = process.env.PASSWORD_SALT;
+
+    const hashPassword = bcrypt.hashSync(password, 16);
+
+    const user = await this._dbContext.user.create({
+      data: {
+        username,
+        password: hashPassword,
+        email,
+        firstName,
+        lastName
+      }
+    });
+
+    return {
+      success: true,
+      message: "User has been created successfully",
+      data: { user }
+    };
   }
 }
